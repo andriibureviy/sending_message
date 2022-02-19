f95e5792108e30c4347608db94802b3776d8e9b4
andriibureviy <andrii.yazvinskyi@gmail.com>
2022-02-19 15:55:48 +0200

    Send any message

--- parser.rb
+++ parser.rb
@@ -52,14 +52,9 @@ end
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-    when 'love'
-      bot.api.send_message(chat_id: message.chat.id,
-                           text: call.sample.sample)
+    case message
+    when Telegram::Bot::Types::Message
+      bot.api.send_message(chat_id: message.chat.id, text: call.sample.sample)
     end
   end
 end
