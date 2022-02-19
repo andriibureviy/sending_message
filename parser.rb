a4afd618c77777b16c317e4a83ac7ace0e21840f
andriibureviy <andrii.yazvinskyi@gmail.com>
2022-02-19 17:53:58 +0200

    Send more message

--- parser.rb
+++ parser.rb
@@ -1,3 +1,4 @@
+
 require 'nokogiri'
 require 'httparty'
 require 'pry'
@@ -8,11 +9,17 @@ TOKEN = '5105333952:AAEkU-w1r3iNKpgH78qjuxF2Z0DQcqYf2A4'
 def call
   adverts = Array.new
 
-  adverts_listings1.each do |adverts_listing|
-    advert =  Array.new
-    advert << adverts_listing.css('p').text
+  adverts_listings2.each do |adverts_listing|
+    # advert =  Array.new
+    advert = adverts_listing.css('p').text.chomp.delete("\n").split('#')
+    advert.each do |i|
+      advert.delete(i) if i.size < 5 || i.size > 30
+    end
     adverts << advert
   end
+
+  a = adverts_listings1.css('p').text.chomp.delete("\n").split('.')
+  adverts <<  a if a.size > 1
 
   adverts_listings.each do |adverts_listing|
     advert = adverts_listing.css('p').text.chomp.delete("\n").split('***') # title
@@ -50,6 +57,17 @@ def adverts_listings1
   parsed_page1.css('div.c')
 end
 
+def parsed_page2
+  link = 'https://dd-restaurant.ru/uk/preserving-peppers/shekspir-krasivye-frazy-uilyam-shekspir-aforizmy-citaty-vyskazyvaniya/'
+  Nokogiri::HTML(HTTParty.get(link).body)
+end
+
+
+def adverts_listings2
+  parsed_page2.css('div.content')
+end
+
+
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message
@@ -58,4 +76,5 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
     end
   end
 end
+
 
