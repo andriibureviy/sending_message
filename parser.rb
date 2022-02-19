493d26df14d1efe06cf3ea4ff767052819cf9f73
andriibureviy <andrii.yazvinskyi@gmail.com>
2022-02-19 14:53:24 +0200

    Up 200 message

--- parser.rb
+++ parser.rb
@@ -6,8 +6,14 @@ require 'telegram/bot'
 TOKEN = '5105333952:AAEkU-w1r3iNKpgH78qjuxF2Z0DQcqYf2A4'
 
 def call
-  parsed_page
   adverts = Array.new
+
+  adverts_listings1.each do |adverts_listing|
+    advert =  Array.new
+    advert << adverts_listing.css('p').text
+    adverts << advert
+  end
+
   adverts_listings.each do |adverts_listing|
     advert = adverts_listing.css('p').text.chomp.delete("\n").split('***') # title
     advert.each do |i|
@@ -15,13 +21,12 @@ def call
     end
     adverts << advert
   end
-  p adverts
+  adverts
 end
 
 def url
   'https://maximum.fm/citati-pro-lyubov-50-krilatih-fraz-i-visloviv-velikih-lyudej-pro-lyubov_n193027'
 end
-
 
 def unparsed_page
   HTTParty.get(url)
@@ -35,6 +40,16 @@ def adverts_listings
   parsed_page.css('div.news-content.news-content-block')
 end
 
+def parsed_page1
+  link = "https://citaty.net.ua/tsytaty-pro-kohannya-na-vidstani-200-tsytat/"
+  Nokogiri::HTML(HTTParty.get(link).body)
+end
+
+
+def adverts_listings1
+  parsed_page1.css('div.c')
+end
+
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message.text
@@ -42,7 +57,7 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-    when '/love'
+    when 'love'
       bot.api.send_message(chat_id: message.chat.id,
                            text: call.sample.sample)
     end
