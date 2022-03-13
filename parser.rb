658e703d532187ed55e624e8309733c057a7c0f4
andriibureviy <andrii.yazvinskyi@gmail.com>
2022-03-14 00:43:43 +0200

    FIX

--- parser.rb
+++ parser.rb
@@ -80,6 +80,7 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message
     when Telegram::Bot::Types::Message
+      Crono.perform(bot.api.send_message(chat_id: message.chat.id, text: call.sample.sample)).every 1.day, at: {hour: 10, min: 00}
       bot.api.send_message(chat_id: message.chat.id, text: call.sample.sample)
     end
   end
